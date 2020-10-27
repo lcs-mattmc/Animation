18cebe5b4d0472533f27df062e6e02a624da00e6
matt <mmcall@lcs.on.ca>
2020-10-27 12:24:45 -0400

    skillets both lines

--- Playground.playground/Pages/skiddles.xcplaygroundpage/Contents.swift
+++ Playground.playground/Pages/skiddles.xcplaygroundpage/Contents.swift
@@ -25,23 +25,57 @@ for x in stride (from: 0, through: 300, by: 50) {
         
         // draw line first, then go every other
         for y in stride(from: 50, through: 250, by: 100){
-        
-        //draw a line
-        canvas.drawLine(from: Point(x: x, y: y), to: Point(x: x, y: y + 50))
+            
+            //draw a line
+            canvas.drawLine(from: Point(x: x, y: y), to: Point(x: x, y: y + 50))
             
         }
     } else {
         // draw a gap first, then go every other
         for y in stride (from: 0, through: 200, by: 100){
-        
-         // draw a line
-        canvas.drawLine(from: Point(x: x, y: y), to: Point(x: x, y: y + 50))
             
-
-    
-    
-    
+            // draw a line
+            canvas.drawLine(from: Point(x: x, y: y), to: Point(x: x, y: y + 50))
+            
+            
+            
+            
+            
+        }
+    }
 }
+
+
+
+
+// generate 7 radom 0s or 1s
+for y in stride (from: 0, through: 300, by: 50) {
+    
+    // generate the random 1 or 0
+    let skiddle = Bool.random ()
+    
+    // when skiddle is (true) 1 we draw first
+    if skiddle == true {
+        
+        // draw line first, then go every other
+        for x in stride(from: 50, through: 250, by: 100){
+            
+            //draw a line
+            canvas.drawLine(from: Point(x: x, y: y), to: Point(x: x + 50, y: y))
+            
+        }
+    } else {
+        // draw a gap first, then go every other
+        for x in stride (from: 0, through: 200, by: 100){
+            
+            // draw a line
+            canvas.drawLine(from: Point(x: x, y: y), to: Point(x: x + 50, y: y ))
+            
+            
+            
+            
+            
+        }
     }
 }
 
